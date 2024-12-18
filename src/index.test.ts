71de7f85d4d6ded149bcfa7105b654f1b1480b60
Lucas Shanley <lpshanley@gmail.com>
2024-12-18 00:39:38 -0600

    Add test function

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,13 @@
 import { expect, suite, test } from 'vitest';
-import { greet } from '.';
+import { greet, VERSION } from '.';
 
 suite('Init Test', () => {
   test('Should create a greeting', () => {
     expect(greet('Lucas')).toBe('Hello Lucas');
   });
+
+  test('Version is 0', () => {
+
+    expect(VERSION).toBe(0)
+  })
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export function greet<TGreeting extends string>(greeting: TGreeting) {
   return `Hello ${greeting}` as const;
 }
+
+export const VERSION = 0
